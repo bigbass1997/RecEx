93bb6651cd1f52fd9f057cb2385b090d1680cdd3
bigbass1997 <bigbass1997.website@gmail.com>
2019-07-23 23:00:04 -0500

    Removed old, unused code. Changed mod version to match the build.gradle file.

--- src/main/java/com/bigbass/recex/RecipeExporterMod.java
+++ src/main/java/com/bigbass/recex/RecipeExporterMod.java
@@ -16,7 +16,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class RecipeExporterMod
 {
 	public static final String MODID = "RecEx";
-	public static final String VERSION = "ALPHA";
+	public static final String VERSION = "0.0.1";
 	
 	public static final Logger log = LogManager.getLogger("RecEx");
 	
